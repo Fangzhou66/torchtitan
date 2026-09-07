fda0938483cffdf6f1b57f8e11387bbb38239007
Fangzhou66 <145536342+Fangzhou66@users.noreply.github.com>
2026-09-07 10:42:32 +0000

    test(tmax): add required license header to resource sizing tests

--- torchtitan/experiments/rl/examples/tmax/evolution/tests/test_latest_resource_sizing.py
+++ torchtitan/experiments/rl/examples/tmax/evolution/tests/test_latest_resource_sizing.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
 """Current TMax peaks must survive every sizing stage without older inputs taking over."""
 
 import importlib.util
